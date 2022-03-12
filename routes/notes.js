e18b6bdc23ac84f07d9646ce6ead29c23cf78315
Steve "Homeschool" Seney <sdseney508@gmail.com>
2022-03-12 17:29:35 -0500

    adding some updates to the server file, added a notes.js; enabled the button from index.html

--- routes/notes.js
+++ routes/notes.js
@@ -1,61 +1,56 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
+const { title } = require('process');
 const file_path = './db/db.json';
 
 //get router for notes in the db.json database
-notes.get('/', (req, res) => {
-    fs.readFile(file_path).then((data) => res.json(JSON.parse(data)));
+notes.get('api/notes', (req, res) => {
+    fs.readFile(file_path, 'utf8', (err,data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Server Error");
+        }
+        else {
+        const json = JSON.parse(data);
+        return json.length > 0
+            ? res.json(json)
+            : res.json('No notes');
+    }
+    });
+ 
 });
 
-//this is for a specific tip.  didn see any requirement for it, but if there is a DELETE
-//there must be a GET
-notes / this.get('/:note_id', (req, res) => {
-    const noteID = req.params.note_id;
-    fs.readFile(file_path)
-        .then((data) => JSON.parse(data))
-        .then((json) => {
-            const result = json.filter((title) => title.note_id === noteID);
-            return result.length > 0
-                ? res.json(result)
-                : res.json('No note with that ID');
-        });
-
-});
 
 //DELETE route for the bonus
-notes.delete('./:note_id', (req, res) => {
+notes.delete('/:note_id', (req, res) => {
     const noteID = req.params.note_id;
-    fs.readFile(file_path)
-        .then((data) => JSON.parse(data))
-        .then((json) => {
-        //use the object filter method to remove the one we want deleted. 
-        const newobj = json.filter((note) => note.note_id !== noteID);
+    fs.readFileSync(file_path);
+    const json = JSON.parse(data);
+    //use the object filter method to remove the one we want deleted. 
+    const newobj = json.filter((note) => note.note_id !== noteID);
 
-        //save the new object back to storage
-        fs.writeFile(file_path, JSON.stringify(newobj, null, 4), (err) =>
-            err ? console.error(err) : console.info(`\nNote ${noteID} was deleted`)
-        );
+    //save the new object back to storage
+    fs.writeFile(file_path, JSON.stringify(newobj, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nNote ${noteID} was deleted`)
+    );
 
-        //give user a response if succesful
-        res.json(`Note ${noteID} was deleted`);
-    });
-
+    //give user a response if succesful
+    res.json(`Note ${noteID} was deleted`);
 });
 
-
 //post route for a new note
-notes.post('/', (req, res) => {
+notes.post('/api/notes', (req, res) => {
     //log it so i can check that everything worked
     console.log(req.body);
-    const parsed_data;
-    const { title, text, note_id } = req.body;
+    let parsed_data = [];
+    const { title, text } = req.body;
 
     if (req.body) {
         const new_note = {
             title,
             text,
-            note_id: uuidv4();
+            note_id: uuidv4()
         };
 
         //now add the new note to the file.  first we read the file; then we
@@ -63,22 +58,61 @@ notes.post('/', (req, res) => {
         fs.readFile(file_path, 'utf-8', (err, data) => {
             if (err) {
                 console.err(err);
+                res.status(500).send("Server Error");
             }
             else {
                 parsed_data = JSON.parse(data);
                 parsed_data.push(new_note);
+                fs.writeFile(file_path, JSON.stringify(parsed_data, null, 4), (err) => {
+                    err ? console.error(err) : console.info(`\nNew Note written to ${file_path}`)
+                    res.status(200).send("Yeah");
+                });
+
             }
-        })
+        });
         //now we write to file
-        fs.writeFile(file_path, JSON.stringify(parsed_data, null, 4), (err) =>
-            err ? console.error(err) : console.info(`\nNew Note written to ${file_path}`)
-        );
     }
     else {
         //log an error for later
         res.error('Something went wrong');
     }
-
 });
 
+notes.put('/api/notes', (req, res) => {
+    //log it so i can check that everything worked
+    console.log(req.body);
+    let parsed_data = [];
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const new_note = {
+            title,
+            text,
+            note_id: uuidv4()
+        };
+
+        //now add the new note to the file.  first we read the file; then we
+        //parse it, append it using .push(data) method, then write to file.
+        fs.readFile(file_path, 'utf-8', (err, data) => {
+            if (err) {
+                console.err(err);
+                res.status(500).send("Server Error");
+            }
+            else {
+                parsed_data = JSON.parse(data);
+                parsed_data.push(new_note);
+                fs.writeFile(file_path, JSON.stringify(parsed_data, null, 4), (err) => {
+                    err ? console.error(err) : console.info(`\nNew Note written to ${file_path}`)
+                    res.status(200).send("Yeah");
+                });
+
+            }
+        });
+        //now we write to file
+    }
+    else {
+        //log an error for later
+        res.error('Something went wrong');
+    }
+});
 module.exports = notes;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 //handle JSON and urlencoded form data
-app.use(express.JSON());
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api', api);
 
